dd03058a52f9db56dfdc5dbd9389c2c580985f16
Shelton-Rolle <sheltonrolle7@gmail.com>
2023-01-25 19:06:21 -0600

    finished styling the grid items

--- src/components/ResultsGridItem.tsx
+++ src/components/ResultsGridItem.tsx
@@ -2,9 +2,22 @@ import { CollectionItem } from "@/data/collection";
 
 export const ResultsGridItem = ({ name, hex }: CollectionItem) => {
     return (
-        <div className="grid-item">
-            <p>{name}</p>
-            <p>{`#${hex}`}</p>
+        <div
+            className="grid-item"
+            style={{
+                border: `2px solid #${hex}`,
+            }}
+        >
+            <div
+                className="grid-item__preview"
+                style={{
+                    backgroundColor: `#${hex}`,
+                }}
+            />
+            <div className="grid-item__content">
+                <h3>{name}</h3>
+                <p>{`#${hex}`}</p>
+            </div>
         </div>
     );
 };
